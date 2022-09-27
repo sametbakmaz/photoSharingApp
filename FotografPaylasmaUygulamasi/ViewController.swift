a4134d035e9b9448f25f148aa3ba176d401afcca
sametbakmaz <sametbkmz@gmail.com>
2022-09-27 12:43:55 +0300

    Initial Commit

---
+++ FotografPaylasmaUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FotografPaylasmaUygulamasi
+//
+//  Created by Abdulsamet Bakmaz on 27.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
